0025b2bb33740b3690be30abf35e6f73da31d8a9
EvanSchisler <eschisler@acromediainc.com>
2016-07-05 16:27:16 -0700

    Issue #2761047: Convert plugins from annotated to yml

---
+++ src/Plugin/BoxBasePluginManagerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\commerce_shipping\Plugin;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Defines an interface for box_base_plugin managers.
+ */
+interface BoxBasePluginManagerInterface extends PluginManagerInterface {
+    // Add getters and other public methods for box_base_plugin managers.
+
+}
---
+++ src/Plugin/PackingSlipBasePluginManagerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\commerce_shipping\Plugin;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Defines an interface for packing_slip_base_plugin managers.
+ */
+interface PackingSlipBasePluginManagerInterface extends PluginManagerInterface {
+    // Add getters and other public methods for packing_slip_base_plugin managers.
+
+}
